3c69d09a02e4232cb63ce9c9b4fe09711b0d7582
Glorphindale <glorphindale@gmail.com>
2012-03-21 22:27:59 +0700

    Refactor code

--- js/Members.js
+++ js/Members.js
@@ -12,6 +12,13 @@ window.addEvent('domready', function () {
         return Math.round(100*value/total);
     };
 
+    function get_value_element(value, total) {
+        var element =
+        '<div class="percentage"><img src="pixel.png" width="' + get_percentage(value, total) + '" height="15" border="1" class="bar">' + +value+' (' + get_percentage(value, total) + '%)</div>';
+        return element;
+    };
+
+
     var total = 1338; // TODO count amount
 
     ["all", "m", "f"].map(function(elem) {
@@ -20,7 +27,7 @@ window.addEvent('domready', function () {
             d.amount = a.amount + b.amount;
             return d;
         }).amount;
-        document.getElementById("sex_" + elem).innerHTML = '<div style="font-size: 10pt; color: #AAAAAA;"><img src="pixel.png" width="'+get_percentage(value, total)+'" height="15" border="1" style="border-color: #00AA00"> '+value+' (' + get_percentage(value, total) + '%)</div>';
+        document.getElementById("sex_" + elem).innerHTML = get_value_element(value, total);
     });
 
     ["all", "50+", "21-50", "16-20", "11-15", "6-10", "2-5", "1"].map(function(elem) {
@@ -29,7 +36,7 @@ window.addEvent('domready', function () {
             d.amount = a.amount + b.amount;
             return d;
         }).amount;
-        document.getElementById("company_" + elem).innerHTML = '<div style="font-size: 10pt; color: #AAAAAA;"><img src="pixel.png" width="'+get_percentage(value, total)+'" height="15" border="1" style="border-color: #00AA00"> '+value+' (' + get_percentage(value, total) + '%)</div>';
+        document.getElementById("company_" + elem).innerHTML = get_value_element(value, total);
     });
 
     ["all", "hr", "mgmt", "qa", "designer", "analysis", "developer", "admin", "student", "na"].map(function(elem) {
@@ -38,7 +45,7 @@ window.addEvent('domready', function () {
             d.amount = a.amount + b.amount;
             return d;
         }).amount;
-        document.getElementById("position_" + elem).innerHTML = '<div style="font-size: 10pt; color: #AAAAAA;"><img src="pixel.png" width="'+get_percentage(value, total)+'" height="15" border="1" style="border-color: #00AA00"> '+value+' (' + get_percentage(value, total) + '%)</div>';
+        document.getElementById("position_" + elem).innerHTML =  get_value_element(value, total);
     });
 });
 
